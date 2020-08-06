c55ca5576f614f0d01c8ed47af5e7d8fba0383f6
Patrick R <pareich@uzh.ch>
2020-08-06 18:38:37 +0200

    Added UserConfig

---
+++ app/src/main/java/com/example/thewheel/UserConfig.java
@@ -0,0 +1,23 @@
+package com.example.thewheel;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class UserConfig extends AppCompatActivity {
+
+
+    private Button spin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+}
