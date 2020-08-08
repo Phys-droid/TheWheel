4d0d2c5aa92c825b7ddd73bfc086a367a16d8709
Phys-droid <qwsatt@gmx.ch>
2020-08-08 11:26:50 +0200

    graphic_concepts

--- app/src/main/java/com/example/thewheel/MainActivity.java
+++ app/src/main/java/com/example/thewheel/MainActivity.java
@@ -2,12 +2,15 @@ package com.example.thewheel;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.content.res.AppCompatResources;
+import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.view.animation.Animation;
@@ -19,6 +22,8 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
+import java.lang.reflect.Field;
+import java.util.ArrayList;
 import java.util.Random;
 
 import static com.example.thewheel.R.drawable.singleplayer;
@@ -26,7 +31,7 @@ import static com.example.thewheel.R.drawable.singleplayer;
 public class MainActivity extends AppCompatActivity {
 
 
-    private ImageView test;
+    private ImageView x;
     private ImageButton option_button;
     private Button button_spin;
     ImageView spinning_part;
@@ -49,21 +54,12 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) { rotationAnimation();
             }
         });
-
-
-        test = (ImageView) findViewById(R.id.test);
-        test.setColorFilter(Color.RED);
-        test.setX(0);
-        test.setY(25);
-
-
         spinning_part = findViewById(R.id.spinning_part);
-
-
-
     }
+
+
     private void rotationAnimation() {
-        RotateAnimation a = new RotateAnimation(0, 180+r.nextInt(), Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+        RotateAnimation a = new RotateAnimation(0, (180+r.nextInt())/1000, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
         ImageView animated = findViewById(R.id.spinning_part);
         a.setInterpolator(new LinearInterpolator());
         int x =  5 + (int)(Math.random() * 10);
@@ -73,16 +69,68 @@ public class MainActivity extends AppCompatActivity {
         animated.startAnimation(a);
     }
 
-    public void openOptions(){
+    public void openOptions() {
         Intent intent = new Intent(this, UserOverview.class);
-
         startActivity(intent);
+    }
+
+    public void construct_wheel(int number_of_players, ArrayList<ImageView> Graphics){  //TODO
+
+    }
+
+     public void animate_wheel(ArrayList<ImageView> Graphics){                          //TODO
+
+    }
+/*
+    public ImageView merge_drawables(ArrayList<ImageView> drawables){                   //TODO
+
+        ImageView a = (ImageButton) findViewById(R.id.button);
+
+        Drawable plusIcon = ContextCompat.getDrawable(this, R.drawable.plus);
+        Drawable dotIcon = ContextCompat.getDrawable(this, R.drawable.oval);
+
+        int horizontalInset = (plusIcon.getIntrinsicWidth() - dotIcon.getIntrinsicWidth()) / 2;
+
+        LayerDrawable finalDrawable = new LayerDrawable(new Drawable[] {plusIcon, dotIcon});
+        finalDrawable.setLayerInset(0, 0, 0, 0, dotIcon.getIntrinsicHeight());
+        finalDrawable.setLayerInset(1, horizontalInset, plusIcon.getIntrinsicHeight(), horizontalInset, 0);
+        a.setImageDrawable(finalDrawable);
+        return a;
 
 
     }
-    String[] names =  {"Dima", "Päde", "Max"};
-    String[] colours = {"RED", "BROWN", "BLUE"};
+*/
+
+
+    public void wheel(UserList Setup) {
+        ArrayList<User> UserArray = Setup.userArray;
+        ArrayList<Bitmap> picture = new ArrayList<>();
+        ArrayList<ImageView> graphics = new ArrayList<>();
+        int number_of_players = UserArray.size();
+
+
+        for (int i=0;i<number_of_players;i++){     //Change Type String to Color in User class
+            /*
+            String a = UserArray.get(i).colour;
+            Color c = new Color(Integer.parseInt(a));
+            */
+
+            x = (ImageView) findViewById(R.id.test);
+            //x.setColorFilter(c);
+            x.setX(0);
+            x.setY(0);
 
 
 
+            graphics.add(x);
+        }
+        construct_wheel(number_of_players, graphics);
+        animate_wheel(graphics);
+
+    }
 }
+
+
+
+
+
--- app/src/main/java/com/example/thewheel/Wheel.java
+++ app/src/main/java/com/example/thewheel/Wheel.java
@@ -2,7 +2,9 @@ package com.example.thewheel;
 /*
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -15,8 +17,14 @@ public class Wheel {
         this.picture = new ArrayList<>();
         int number_of_players = UserList.size();
 
+        private ImageView test;
         for (int i=0;i<number_of_players;i++){
-            this.picture.add(BitmapFactory.decodeResource(res, R.drawable.number_of_players));
+            if (number_of_players == 1){
+
+                test = (ImageView) findViewById(R.id.test);
+                test.setColorFilter(Color.RED);
+                test.setX(0);
+                test.setY(0);
             }
 
 
