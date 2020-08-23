6883360b1d927e45de3629b4a621b8553b58ea93
Phys-droid <qwsatt@gmx.ch>
2020-08-23 20:09:12 +0200

    code_cleanup

--- app/src/main/java/com/example/thewheel/MainActivity.java
+++ app/src/main/java/com/example/thewheel/MainActivity.java
@@ -172,7 +172,6 @@ public class MainActivity<start> extends AppCompatActivity {
 
 
             for (int i = 0; i < values.length; i++) {//values2.length; i++) {
-                if (i == i) {
                     paint.setColor(COLORS[i]);
                     UserList userList = SetupList.getCurrentUserList();
                     if (userList != null){
@@ -200,20 +199,8 @@ public class MainActivity<start> extends AppCompatActivity {
 
                     temp += (int) values[i];
 
-                }
-                else
-                {
-                    temp += (int) values[i - 1];
-                    paint.setColor(COLORS[i]);
-                    if (COLORS[i] == Color.BLACK){
-                        paint2.setColor(Color.WHITE);
-                    }
-                    canvas.drawArc(rectf, temp + angle, values[i], true, paint);
-                    alpha = (float) ((temp+angle+(values[i]/2)));
-                    canvas.rotate(alpha+2, size/2, size/2);
-                    canvas.drawText(names[i],size/2+200, size/2, paint2);
-                    canvas.rotate(-alpha-2, size/2, size/2);
-                }
+
+
             }
         }
 
